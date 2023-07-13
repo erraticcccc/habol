16da99afcce6f092a491807fd9b7f4b43844d736
erraticcccc <78512572+erraticcccc@users.noreply.github.com>
2023-07-13 15:23:25 -0700

    Create face.lua

---
+++ face.lua
@@ -0,0 +1,16 @@
+local faces = {
+	"models/gman/gman_face_map3",
+	"models/player/hvyweapon/heavy_head_zombie",
+	"models/player/engineer/engineer_head_zombie",
+	"models/monk/grigori_head",
+	"models/chell/chell_rev2_head",
+}
+
+timer.Create("face", 20, 0, function()
+
+	LocalPlayer():ConCommand("pp_mat_overlay " .. table.Random(faces))
+	timer.Simple(0.1, function()
+		LocalPlayer():ConCommand("pp_mat_overlay ''")
+	end)
+
+end)
