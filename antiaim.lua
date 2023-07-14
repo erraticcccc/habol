bd58276a3e1a7079c9426d744d8cf86fb3e7e7cb
erraticcccc <78512572+erraticcccc@users.noreply.github.com>
2023-07-14 10:26:28 -0700

    Update antiaim.lua

--- antiaim.lua
+++ antiaim.lua
@@ -1,20 +1,15 @@
-local _m = math
-local _h = hook
-local _t = timer
-local function oClamp(num,min,max) -- short for Overflow Clamp.
-	if num < min then return max end
-	if num > max then return min end
-	return num
-end
+local _m = table.Copy(math)
+local _h = table.Copy(hook)
+local _t = table.Copy(timer)
 
 local oAngles
 local currTarget
 _h.Add('CreateMove','testingfunny',function(cmd)
-	currTarget = LocalPlayer():GetEyeTrace()
-	if currTarget.Entity:IsPlayer() or currTarget.Entity:IsNPC() then
-		oAngles = cmd:GetViewAngles()
-		cmd:SetViewAngles(Angle((_m.Clamp(oAngles.p+_m.Rand(-8,8),-89,89)),(oClamp(oAngles.y+_m.Rand(-8,8),-179,179)),oAngles.r)) -- Angle(pitch,yaw,roll)
-	end
+    currTarget = LocalPlayer():GetEyeTrace()
+    if currTarget.Entity:IsPlayer() or currTarget.Entity:IsNPC() then
+        oAngles = cmd:GetViewAngles()
+        local newAngles = Angle(oAngles.x + _m.Rand(-8,8),oAngles.y + _m.Rand(-8,8),oAngles.z)
+        newAngles:Normalize()
+        cmd:SetViewAngles(newAngles) -- Angle(pitch,yaw,roll)
+    end
 end)
-
---_t.Simple(200,function() hook.Remove('CreateMove','testingfunny') end)
